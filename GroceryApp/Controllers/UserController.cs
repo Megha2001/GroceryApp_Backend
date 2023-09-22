533b8889daa47c09e1aef98f61a7ce815d0e836d
Megha2001 <tmegha2001@gmail.com>
2023-09-22 10:29:49 +0530

    Added User service

---
+++ GroceryApp/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using GroceryApp.Models;
+using GroceryApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService userService;
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Users user)
+        {
+            var data = userService.RegisterUser(user);
+            return Ok(data);
+        }
+
+        [HttpGet]
+        public IActionResult Get(string email, string password)
+        {
+            var data = userService.LoginUser(email, password);
+            if (data == null)
+                return NotFound();
+            return Ok(data);
+        }
+    }
+}
---
+++ GroceryApp/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using GroceryApp.Models;
+namespace GroceryApp.Services
+{
+    public interface IUserService
+    {
+        public Users RegisterUser(Users user);
+        public Users LoginUser(string email, string password);
+    }
+}
